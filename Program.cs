7be7075179d2631f30c21ed4ccebaf8215188e7c
Sam <sam@boers.family>
2024-01-19 08:52:57 +0100

    .

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace OcelotApiGateway
                     {
                         options.AddPolicy("CorsPolicy",
                             builder => builder
-                                .WithOrigins("http://localhost:5173", "http://127.0.0.1:5000")
+                                .WithOrigins("http://localhost:5000")
                                 .AllowAnyMethod()
                                 .AllowAnyHeader()
                                 .AllowCredentials());
